e2b704e72833523c7e628c0904bb47db93520742
John Blance <john.blance@gmail.com>
2020-06-21 20:56:49 +1200

    undo ++

--- powermon/io/testio.py
+++ powermon/io/testio.py
@@ -8,7 +8,7 @@ log = logging.getLogger('powermon')
 
 class TestIO(BaseIO):
     def __init__(self) -> None:
-        self._test_data = bytes("(230.0 50.0 0030 42.0 54.0 56.4 46.0 60 0 0 2 0 0 0 0 0 1 1 0 0 1 0 54.0 0 1 000\x9E\x60\r", 'utf-8')
+        self._test_data = "(230.0 50.0 0030 42.0 54.0 56.4 46.0 60 0 0 2 0 0 0 0 0 1 1 0 0 1 0 54.0 0 1 000\x9E\x60\r"
         self._counter = 0
 
     def read_available(self) -> bytes:
@@ -16,7 +16,7 @@ class TestIO(BaseIO):
         self._counter += 1
         if self._counter > len(self._test_data):
             self._counter = 0
-        return result
+        return bytes(result, 'utf-8')
 
     def write(self, data: bytes) -> None:
         # self._test_data = data
