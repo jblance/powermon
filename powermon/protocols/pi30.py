46d73eafcb4b70910e6bd27aefc7b7e8c032beb3
John Blance <john.blance@gmail.com>
2020-06-24 11:36:03 +1200

    add decode for pi30

--- powermon/protocols/pi30.py
+++ powermon/protocols/pi30.py
@@ -1,4 +1,5 @@
 import logging
+import re
 
 from .protocol import AbstractProtocol
 
@@ -68,45 +69,124 @@ COMMANDS = [
 
 class pi30(AbstractProtocol):
     def __init__(self, *args, **kwargs) -> None:
-        self._protocol_id = 'PI30'
+        super().__init__(self, *args, **kwargs)
+        self._protocol_id = b'PI30'
         log.info(f'Using protocol {self._protocol_id}')
-        self.__command = None
-        self.__show_raw = None
 
-    def get_protocol_id(self):
-        return self._protocol_id
-
-    def set_command(self, command, show_raw=None):
+    def set_command(self, command, show_raw=None) -> None:
+        # Called from get_full_command
         self.__command = command
         self.__show_raw = show_raw
+        self.__command_defn = self.get_command_defn(command)
 
-    def is_known_command(self) -> bool:
+    def get_command_defn(self, command):
         if self.__command is None:
-            log.info('is_known_command called with self._current_command = None')
-            return False
+            return None
         for _command in COMMANDS:
+            if 'regex' in _command and _command['regex']:
+                _re = re.compile(_command['regex'])
+                match = _re.match(command)
+                if match:
+                    log.debug("Matched: {} Value: {}".format(command.name, match.group(1)))
+                    self.__command_value = match.group(1)
+                    return _command
             if _command['name'] == self.__command:
                 log.debug(f'Found command {self.__command} in protocol {self._protocol_id}')
-                return True
-        return False
+                return _command
+        return None
 
-    def get_full_command(self) -> bytes:
+    def get_full_command(self, command, show_raw) -> bytes:
+        self.set_command(command, show_raw)
         byte_cmd = bytes(self.__command, 'utf-8')
         # calculate the CRC
         crc_high, crc_low = self.crc(byte_cmd)
         # combine byte_cmd, CRC , return
         full_command = byte_cmd + bytes([crc_high, crc_low, 13])
-        log.debug('full command: %s', full_command)
+        log.debug(f'full command: {full_command}')
         return full_command
 
     def decode(self, response):
         if self.__show_raw:
             log.debug('Protocol "{self._protocol_id}" raw response requested')
-            return response[:-3]
+            return response
         # Check for a stored command definition
-        if not self.is_known_command():
+        if not self.__command_defn:
             # No definiution, so just return the data
             # TODO: possibly return something better (maybe a dict with 'value_1 etc?')
             return response[1:-3]
         # Decode response based on stored command definition
-        
+        # TODO: finish this bit
+        msgs = {}
+
+        if response is None:
+            log.info('No response')
+            msgs['error'] = ['No response', '']
+            return msgs
+        if not self.is_response_valid(response):
+            log.info('Invalid response')
+            msgs['error'] = ['Invalid response', '']
+            msgs['response'] = [response, '']
+            return msgs
+        if self.__command_defn is None:
+            log.info('No byte_response definition')
+            msgs['error'] = ['No byte_response definition', '']
+            msgs['response'] = [response, '']
+            return msgs
+
+        # Omit the CRC and convert to string
+        # response = self.getResponse()
+        # responses = response.split(" ")
+        responses = self.get_responses()
+        for i, result in enumerate(responses):
+            # Check if we are past the 'known' responses
+            if (i >= len(self.__command_defn['response'])):
+                resp_format = ['string', 'Unknown value in byte_response', '']
+            else:
+                resp_format = self.__command_defn['response'][i]
+
+            key = '{}'.format(resp_format[1]).lower().replace(" ", "_")
+            # Process results
+            if (resp_format[0] == 'float'):
+                msgs[key] = [float(result), resp_format[2]]
+            elif (resp_format[0] == 'int'):
+                msgs[key] = [int(result), resp_format[2]]
+            elif (resp_format[0] == 'string'):
+                msgs[key] = [result, resp_format[2]]
+            elif (resp_format[0] == '10int'):
+                msgs[key] = [float(result) / 10, resp_format[2]]
+            # eg. ['option', 'Output source priority', ['Utility first', 'Solar first', 'SBU first']],
+            elif (resp_format[0] == 'option'):
+                msgs[key] = [resp_format[2][int(result)], '']
+            # eg. ['keyed', 'Machine type', {'00': 'Grid tie', '01': 'Off Grid', '10': 'Hybrid'}],
+            elif (resp_format[0] == 'keyed'):
+                msgs[key] = [resp_format[2][result], '']
+            # eg. ['flags', 'Device status', [ 'is_load_on', 'is_charging_on' ...
+            elif (resp_format[0] == 'flags'):
+                for j, flag in enumerate(result):
+                    msgs[resp_format[2][j]] = [int(flag), 'True - 1/False - 0']
+            # eg. ['stat_flags', 'Warning status', ['Reserved', 'Inver...
+            elif (resp_format[0] == 'stat_flags'):
+                output = ''
+                for j, flag in enumerate(result):
+                    if (flag == '1'):
+                        output = ('{}\n\t- {}'.format(output,
+                                                      resp_format[2][j]))
+                msgs[key] = [output, '']
+            # eg. ['enflags', 'Device Status', {'a': {'name': 'Buzzer', 'state': 'disabled'},
+            elif (resp_format[0] == 'enflags'):
+                # output = {}
+                status = 'unknown'
+                for item in result:
+                    if (item == 'E'):
+                        status = 'enabled'
+                    elif (item == 'D'):
+                        status = 'disabled'
+                    else:
+                        # output[resp_format[2][item]['name']] = status
+                        msgs[resp_format[2][item]['name']] = [status, '']
+                # msgs[key] = [output, '']
+            elif self.__command_defn['type'] == 'SETTER':
+                msgs[self.__command_defn['name']] = [result, '']
+            else:
+                msgs[i] = [result, '']
+        return msgs
