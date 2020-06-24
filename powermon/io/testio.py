bd80b03a1a9a21c0224a8b278774145a9cae0f71
John Blance <john.blance@gmail.com>
2020-06-25 11:52:11 +1200

    use bytes for test data

--- powermon/io/testio.py
+++ powermon/io/testio.py
@@ -9,7 +9,7 @@ log = logging.getLogger('powermon')
 
 class TestIO(BaseIO):
     def __init__(self) -> None:
-        self._test_data = "(230.0 50.0 0030 42.0 54.0 56.4 46.0 60 0 0 2 0 0 0 0 0 1 1 0 0 1 0 54.0 0 1 000\x9E\x60\r"
+        self._test_data = b"(230.0 50.0 0030 42.0 54.0 56.4 46.0 60 0 0 2 0 0 0 0 0 1 1 0 0 1 0 54.0 0 1 000\x9E\x60\r"
         self._counter = 0
 
     def send_and_receive(self, command, show_raw, protocol) -> dict:
@@ -20,6 +20,7 @@ class TestIO(BaseIO):
         if command_defn is not None:
             self._test_data = command_defn['test_responses'][random.randrange(len(command_defn['test_responses']))]
         response = self._test_data
+        response = b"(PI30\x9a\x0b\r"
         log.debug(f'Raw response {response}')
         decoded_response = protocol.decode(response)
         # _response = response.decode('utf-8')
