f6ad27ca61221a24687b4293d0188c1bde3af7e4
John Blance <john.blance@gmail.com>
2020-06-25 12:46:30 +1200

    add f & QPGS to pi30, change COMMANDS to dict

--- powermon/devices/mppsolar.py
+++ powermon/devices/mppsolar.py
@@ -14,22 +14,12 @@ class mppsolar(AbstractDevice):
         log.debug(f'mppsolar __init__ args {args}')
         log.debug(f'mppsolar __init__ kwargs {kwargs}')
 
-    def set_protocol(self, protocol=None) -> None:
-        log.debug(f'mppsolar.set_protocol protocol={protocol}')
-        super().set_protocol(protocol)
-        if self._protocol is None:
-            log.error('Error setting protocol')
-            return
-
-        crc_high, crc_low = self._protocol.crc('test')
-        log.debug(f'test {crc_high:#04x} {crc_low:#04x}')
-
     def run_command(self, command, show_raw=False) -> dict:
         '''
         mpp-solar specific method of running a 'raw' command, e.g. QPI or PI
         '''
         log.info(f'Running command {command}')
-        # TODO: implement protocol determiniation??
+        # TODO: implement protocol self determiniation??
         if self._protocol is None:
             log.error('Attempted to run command with no protocol defined')
             return {'error': 'Attempted to run command with no protocol defined'}
@@ -39,16 +29,4 @@ class mppsolar(AbstractDevice):
 
         response = self._port.send_and_receive(command, show_raw, self._protocol)
         log.debug(f'Send and Receive Response {response}')
-
-        # full_command = self._protocol.get_full_command(command, show_raw)
-        # log.info(f'full command {full_command} for command {command}')
-        # Send the full command via the communications port
-        # self._port.write(full_command)
-        # Get the response from the communications port
-        # TODO: sort async port read
-        # response = self._port.read(10)
-        # decoded_response = self._protocol.decode(response)
-        # _response = response.decode('utf-8')
-        # log.info(f'Raw response {response}')
-        # log.info(f'Decoded response {decoded_response}')
         return response
