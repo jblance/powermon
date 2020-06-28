154bb5c62bb5c0d8f4650722ab08a0db8a7d6e27
John Blance <john.blance@gmail.com>
2020-06-29 11:28:00 +1200

    add initial pi41 definition

---
+++ powermon/protocols/pi41.py
@@ -0,0 +1,13 @@
+import logging
+
+from .pi30 import pi30
+
+log = logging.getLogger('powermon')
+
+
+class pi41(pi30):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__()
+        self._protocol_id = b'PI41'
+        # self.COMMANDS = COMMANDS
+        log.info(f'Using protocol {self._protocol_id} with {len(self.COMMANDS)} commands')
