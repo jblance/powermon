70364e6c115dfe51f29969c7005b61a5124a95e6
jblance <john.blance@gmail.com>
2024-08-12 22:04:16 +1200

    update to cli with more ble detail

--- powermon/cli.py
+++ powermon/cli.py
@@ -1,5 +1,6 @@
 """cli.py - hold classes for the powermon cli"""
 import logging
+import yaml
 from argparse import ArgumentParser
 from platform import python_version
 
@@ -11,6 +12,38 @@ FORMAT = "%(asctime)-15s:%(levelname)s:%(module)s:%(funcName)s@%(lineno)d: %(mes
 logging.basicConfig(format=FORMAT)
 
 
+async def print_bledevice(bledevice, advertisementdata=None, address=None, getChars=False):
+    from bleak import BleakScanner, BLEDevice, BleakClient
+    if (address is not None and bledevice.address != address.upper()):
+        return
+    print("Name:", bledevice.name)
+    print("Address:", bledevice.address)
+    print("Metadata:", yaml.dump(bledevice._metadata))
+    print("RSSI:", bledevice._rssi)
+    print("Details:")
+    print(yaml.dump(bledevice.details, default_flow_style=False))
+    if advertisementdata:
+        print("\tAdvertisementData:")
+        print("\tlocal_name:", advertisementdata.local_name)
+        print("\tmanufacturer_data:", advertisementdata.manufacturer_data)
+        print("\tplatform_data:", advertisementdata.platform_data)
+        print("\tservice_data:", advertisementdata.service_data)
+        print("\tservice_uuids:", advertisementdata.service_uuids)
+        print("\trssi:", advertisementdata.rssi)
+        print("\ttx_power:", advertisementdata.tx_power)
+    if getChars:
+        client=BleakClient(bledevice)
+        print('connecting to BLE client')
+        await client.connect()
+        print("Connected?", client.is_connected)
+        for _int in client.services.characteristics:
+            _char =  client.services.characteristics[_int]
+            #print(yaml.dump(_char))
+            print(f"Characteristic:: Handle: {_int:02d} (0x{_int:02X}), UUID: {_char.uuid}, Description: {_char.description}, Properties: {_char.properties}")
+            for _desc in _char.descriptors:
+                #print(yaml.dump(_desc))
+                print(f"\t Descriptor:: Handle: {_desc.handle:02d} (0x{_desc.handle:02X}), UUID: {_desc.uuid}, Value: {_desc.obj['Value']}")
+
 def main():
     """main entry point for the powermon cli
     """
@@ -18,7 +51,10 @@ def main():
     parser = ArgumentParser(description=description)
 
     parser.add_argument("-v", "--version", action="store_true", help="Display the version")
-    parser.add_argument("--bleScan", action="store_true", help="Scan for BLW devices")
+    parser.add_argument("--bleScan", action="store_true", help="Scan for BLE devices")
+    parser.add_argument("--advData", action="store_true", help="Include advertisement data in BLE Scan")
+    parser.add_argument("--getChars", action="store_true", help="COnnect to BLE device(s) and list characteristics")
+    parser.add_argument("--address", type=str, default=None, help="Only scan for supplied mac address")
 
     args = parser.parse_args()
 
@@ -30,13 +66,23 @@ def main():
 
     if args.bleScan:
         import asyncio
-        from bleak import BleakScanner
+        from bleak import BleakScanner, BLEDevice, BleakClient
 
 
-        async def scan_function():
-            devices = await BleakScanner.discover()
+        async def scan_function(adv_data=False):
+            print("Scanning for BLE devices")
+            devices = await BleakScanner.discover(return_adv=adv_data)
+            print(f"Found {len(devices)} BLE devices")
             for d in devices:
-                print(d)
+                if isinstance(d, BLEDevice):
+                    # d is a BLEDevice
+                    await print_bledevice(bledevice=d, address=args.address, getChars=args.getChars)
+                elif isinstance(d, str):
+                    # d is key to BLEDevice, Advertisement tuple
+                    _bledevice, _advertisementdata = devices[d]
+                    await print_bledevice(bledevice=_bledevice, advertisementdata=_advertisementdata, address=args.address, getChars=args.getChars)
+                else:
+                    print("unknown d")
 
 
-        asyncio.run(scan_function())
+        asyncio.run(scan_function(args.advData))
