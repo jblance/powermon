75479f33e5e2e8bd11b801c02be265c48c771f64
John Blance <1266998+jblance@users.noreply.github.com>
2024-08-16 03:24:13 +0000

    add cell_voltages test

--- tests/unit/test_protocol_daly.py
+++ tests/unit/test_protocol_daly.py
@@ -91,3 +91,18 @@ class TestProtocolDaly(unittest.TestCase):
         formatted_data = simple_formatter.format(command, _result, device_info)
         # print(formatted_data)
         self.assertEqual(formatted_data, expected)
+
+
+    def test_build_result_cell_voltages(self):
+        """ test result build for cell voltages """
+        expected = ['cell_01_voltage=3.413V', 'cell_02_voltage=3.396V', 'cell_03_voltage=3.406V', 'cell_04_voltage=3.425V', 'cell_05_voltage=3.42V', 'cell_06_voltage=3.404V', 'cell_07_voltage=3.402V', 'cell_08_voltage=3.414V', 'cell_09_voltage=3.412V', 'cell_10_voltage=3.417V', 'cell_11_voltage=3.422V', 'cell_12_voltage=3.426V', 'cell_13_voltage=3.41V', 'cell_14_voltage=3.422V', 'cell_15_voltage=3.416V', 'cell_16_voltage=3.403V', 'cell_24_voltage=0.165V']
+        raw_response = b'\xa5\x01\x95\x08\x01\rU\rD\rN\x89\xdb\xa5\x01\x95\x08\x02\ra\r\\\rL\x89\xfe\xa5\x01\x95\x08\x03\rJ\rV\rT\x89\xea\xa5\x01\x95\x08\x04\rY\r^\rb\x89\x10\xa5\x01\x95\x08\x05\rR\r^\rX\x89\x00\xa5\x01\x95\x08\x06\rK\x00\x00\x00\x00\x89*\xa5\x01\x95\x08\x07\x00\x00\x00\x00\x00\x00\x89\xd3\xa5\x01\x95\x08\x08\x00\x00\x00\x00\x00\xa5\x01\x95\x08\t\x00\x00\x00\x00\x00\x00\x89\xd5\xa5\x01\x95\x08\n\x00\x00\x00\x00\x00\x00\x89\xd6\xa5\x01\x95\x08\x0b\x00\x00\x00\x00\x00\x00\x89\xd7\xa5\x01\x95\x08\xa8\x00@\x00\x00 @\x00\r0\x00\x00\x00 @\x00\xf3Z\x00\x00m2\x00\x00\x00 @\x00S1\x00\x00\x00\x00\x00\xa5\x01\x95\x08\x0f\x00\x00\x00\x00\x00\x00\x89\xdb\xa5\x01\x95\x08\x10\x00\x00\x00\x00\x00\x00\x89\xdc'
+        simple_formatter = SimpleFormat({"extra_info": True})
+        device_info = DeviceInfo(name="name", device_id="device_id", model="model", manufacturer="manufacturer")
+        command = Command.from_config({"command": "cell_voltages"})
+        command.command_definition = proto.get_command_definition('cell_voltages')
+        _result = command.build_result(raw_response=raw_response, protocol=proto)
+        formatted_data = simple_formatter.format(command, _result, device_info)
+        # print(formatted_data)
+        self.assertEqual(formatted_data, expected)
+
