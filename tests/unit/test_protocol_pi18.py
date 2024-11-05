649dbedaa78652fc96a18404c990c170148abef9
John Blance <1266998+jblance@users.noreply.github.com>
2024-11-05 22:06:46 +0000

    add vfw and flag

--- tests/unit/test_protocol_pi18.py
+++ tests/unit/test_protocol_pi18.py
@@ -22,14 +22,15 @@ class TestProtocolPI18(unittest.TestCase):
     def test_check_crc_incorrect(self):
         """ test a for failing CRC validation (crc is wrong) """
         response = b"^D0882300,217,2300,500,217,5000,5000,480,480,530,440,570,570,2,10,070,1,1,1,9,0,0,0,0,1,00\xe2k\r"
-        result = proto.check_crc(response=response, command_definition=proto.get_command_definition('PIRI'))
-        self.assertFalse(result)
+        #result = proto.check_crc(response=response, command_definition=proto.get_command_definition('PIRI'))
+        self.assertRaises(InvalidCRC, proto.check_crc, response=response, command_definition=proto.get_command_definition('PIRI'))
 
     def test_check_crc_wrong_response_start(self):
         """ test a for failing CRC validation (not a response that starts with ^D) """
         response = b"0882300,217,2300,500,217,5000,5000,480,480,530,440,570,570,2,10,070,1,1,1,9,0,0,0,0,1,00\xe1k\r"
-        result = proto.check_crc(response=response, command_definition=proto.get_command_definition('PIRI'))
-        self.assertFalse(result)
+        # result = proto.check_crc(response=response, command_definition=proto.get_command_definition('PIRI'))
+        self.assertRaises(InvalidResponse, proto.check_crc, response=response, command_definition=proto.get_command_definition('PIRI'))
+        # self.assertFalse(result)
 
     def test_get_full_command_piri(self):
         """ test full command generation for PIRI command """
@@ -64,7 +65,7 @@ class TestProtocolPI18(unittest.TestCase):
         device_info = DeviceInfo(name="name", serial_number="serial_number", model="model", manufacturer="manufacturer")
         command = Command.from_config({"command": "POP1"})
         command.command_definition = proto.get_command_definition('POP1')
-        _result = command.build_result(raw_response=b"^0\x0b\xe3\r", protocol=proto)
+        _result = command.build_result(raw_response=b"^0\x1b\xe3\r", protocol=proto)
         formatted_data = simple_formatter.format(command, _result, device_info)
         # print(formatted_data)
         self.assertEqual(formatted_data, expected)
@@ -85,6 +86,36 @@ class TestProtocolPI18(unittest.TestCase):
         command = Command.from_config({"command": "MCHGV552,540"})
         command.command_definition = proto.get_command_definition('MCHGV552,540')
         _result = command.build_result(raw_response=b"^1\x0b\xc2\r", protocol=proto)
+        formatted_data = simple_formatter.format(command, _result, device_info)
+        # print(formatted_data)
+        self.assertEqual(formatted_data, expected)
+
+    def test_check_crc_flag(self):
+        """ test a for correct CRC validation """
+        response = b"^D0200,0,0,0,0,1,0,0,12\xc2\x39\r"
+        result = proto.check_crc(response=response, command_definition=proto.get_command_definition('FLAG'))
+        self.assertTrue(result)
+
+    def test_build_result_flag(self):
+        """ test result build for flag - success """
+        expected = ['buzzer_beep=disabled', 'overload_bypass_function=disabled', 'display_back_to_default_page=disabled', 'overload_restart=disabled', 'over_temperature_restart=disabled', 'backlight_on=enabled', 'alarm_primary_source_interrupt=disabled', 'fault_code_record=disabled', 'reserved=12']
+        simple_formatter = SimpleFormat({"extra_info": False})
+        device_info = DeviceInfo(name="name", serial_number="serial_number", model="model", manufacturer="manufacturer")
+        command = Command.from_config({"command": "FLAG"})
+        command.command_definition = proto.get_command_definition('FLAG')
+        _result = command.build_result(raw_response=b"^D0200,0,0,0,0,1,0,0,12\xc2\x39\r", protocol=proto)
+        formatted_data = simple_formatter.format(command, _result, device_info)
+        # print(formatted_data)
+        self.assertEqual(formatted_data, expected)
+
+    def test_build_result_vfw(self):
+        """ test result build for vfw """
+        expected = ['main_cpu_version=5220', 'slave_1_cpu_version=0', 'slave_2_cpu_version=0']
+        simple_formatter = SimpleFormat({"extra_info": False})
+        device_info = DeviceInfo(name="name", serial_number="serial_number", model="model", manufacturer="manufacturer")
+        command = Command.from_config({"command": "VFW"})
+        command.command_definition = proto.get_command_definition('VFW')
+        _result = command.build_result(raw_response=b"^D02005220,00000,00000\x3e\xf8\r", protocol=proto)
         formatted_data = simple_formatter.format(command, _result, device_info)
         # print(formatted_data)
         self.assertEqual(formatted_data, expected)
