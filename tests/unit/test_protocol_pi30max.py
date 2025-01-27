4211d9acfeb2844f5d574341ca2d6d4bcca29c8f
John Blance <1266998+jblance@users.noreply.github.com>
2025-01-27 03:42:00 +0000

    add extended pi30 protocol tests

---
+++ tests/unit/test_protocol_pi30max.py
@@ -0,0 +1,85 @@
+""" tests / pmon / unit / test_protocol_pi30max.py """
+import unittest
+
+from powermon.commands.command import Command
+from powermon.device import DeviceInfo
+from powermon.libs.errors import InvalidCRC, InvalidResponse
+from powermon.outputformats.simple import SimpleFormat
+from powermon.protocols import get_protocol_definition
+# proto = get_protocol_definition('PI30MAXA')  # uses pi30 with max model
+proto = get_protocol_definition('PI30MAX')   # uses full pi30max class file
+
+
+class TestProtocolPi30Max(unittest.TestCase):
+    """ exercise different functions in PI30MAX protocol """
+
+    def test_full_command_qvfw(self):
+        """ test a for correct full command for QVFW """
+        _result = proto.get_full_command(command="QVFW")
+        expected = b'QVFWb\x99\r'
+        # print(_result)
+        self.assertEqual(_result, expected)
+
+    def test_build_result_qvfw(self):
+        """ test result build for QVFW """
+        expected = ['main_cpu_firmware_version=00072.70']
+        simple_formatter = SimpleFormat({"extra_info": True})
+        device_info = DeviceInfo(name="name", serial_number="serial_number", model="model", manufacturer="manufacturer")
+        command = Command.from_config({"command": "QVFW"})
+        command.command_definition = proto.get_command_definition('QVFW')
+        _result = command.build_result(raw_response=b"(VERFW:00072.70\x53\xA7\r", protocol=proto)
+        formatted_data = simple_formatter.format(command, _result, device_info)
+        # print(formatted_data)
+        self.assertEqual(formatted_data, expected)
+
+    def test_build_result_qvfw_incorrect_crc(self):
+        """ test result build for QVFW with an incorrect CRC"""
+        expected = ['Error Count: 1',
+            "Error #0: response has invalid CRC - got '\\x53\\xa6', calculated '\\x53\\xa7'"]
+        simple_formatter = SimpleFormat({"extra_info": True})
+        device_info = DeviceInfo(name="name", serial_number="serial_number", model="model", manufacturer="manufacturer")
+        command = Command.from_config({"command": "QVFW"})
+        command.command_definition = proto.get_command_definition('QVFW')
+        _result = command.build_result(raw_response=b"(VERFW:00072.70\x53\xA6\r", protocol=proto)
+        formatted_data = simple_formatter.format(command, _result, device_info)
+        # print(formatted_data)
+        self.assertEqual(formatted_data, expected)
+
+    def test_get_id(self):
+        """ test get id command - should return QID """
+        expected = "QSID"
+        res = proto.get_command_definition("get_id")
+        # print(res.code)
+        self.assertEqual(res.code, expected)
+
+    def test_build_result_QPIGS2(self):
+        """ test result build for QPIGS2 """
+        expected = ['pv2_input_current=3.1A', 'pv2_input_voltage=327.3V', 'pv2_charging_power=1026W']
+        simple_formatter = SimpleFormat({"extra_info": False})
+        device_info = DeviceInfo(name="name", serial_number="serial_number", model="model", manufacturer="manufacturer")
+        command = Command.from_config({"command": "QPIGS2"})
+        command.command_definition = proto.get_command_definition('QPIGS2')
+        _result = command.build_result(raw_response=b"(03.1 327.3 01026 \xc9\x8b\r", protocol=proto)
+        formatted_data = simple_formatter.format(command, _result, device_info)
+        # print(formatted_data)
+        self.assertEqual(formatted_data, expected)
+
+    def test_build_result_QPIRI(self):
+        """ test result build for QPIRI """
+        expected = ['ac_input_voltage=230.0V', 'ac_input_current=21.7A', 'ac_output_voltage=230.0V', 'ac_output_frequency=50.0Hz', 'ac_output_current=21.7A', 'ac_output_apparent_power=5000VA', 'ac_output_active_power=4000W', 'battery_voltage=48.0V', 'battery_recharge_voltage=46.0V', 'battery_under_voltage=42.0V', 'battery_bulk_charge_voltage=56.4V', 'battery_float_charge_voltage=54.0V', 'battery_type=AGM', 'max_ac_charging_current=10A', 'max_charging_current=10A', 'input_voltage_range=UPS', 'output_source_priority=Utility Solar Battery', 'charger_source_priority=Utility first', 'max_parallel_units=6', 'machine_type=Off Grid', 'topology=transformerless', 'output_mode=single machine', 'battery_redischarge_voltage=54.0V', 'pv_ok_condition=As long as one unit of inverters has connect PV, parallel system will consider PV OK', 'pv_power_balance=PV input max power will be the sum of the max charged power and loads power']
+        simple_formatter = SimpleFormat({"extra_info": False})
+        device_info = DeviceInfo(name="name", serial_number="serial_number", model="model", manufacturer="manufacturer")
+        command = Command.from_config({"command": "QPIRI"})
+        command.command_definition = proto.get_command_definition('QPIRI')
+        _result = command.build_result(raw_response=b"(230.0 21.7 230.0 50.0 21.7 5000 4000 48.0 46.0 42.0 56.4 54.0 0 10 010 1 0 0 6 01 0 0 54.0 0 1\x6F\x7E\r", protocol=proto)
+        formatted_data = simple_formatter.format(command, _result, device_info)
+        # print(formatted_data)
+        self.assertEqual(formatted_data, expected)
+
+    def test_battery_options_QPIRI(self):
+        """ test battery options list of QPIRI """
+        expected = ['AGM', 'Flooded', 'User', 'Pylontech', 'Shinheung', 'WECO', 'Soltaro', 'TBD', 'LIb-protocol compatible', '3rd party Lithium']
+        command_definition = proto.get_command_definition('QPIRI')
+        battery_options = command_definition.reading_definitions[12].options
+        # print(battery_options)
+        self.assertListEqual(expected, battery_options)
---
+++ tests/unit/test_protocol_pi30mst.py
@@ -0,0 +1,43 @@
+""" tests / pmon / unit / test_protocol_pi30.py """
+import unittest
+
+from powermon.commands.command import Command
+from powermon.device import DeviceInfo
+from powermon.libs.errors import InvalidCRC, InvalidResponse
+from powermon.outputformats.simple import SimpleFormat
+from powermon.protocols import get_protocol_definition
+
+# import construct as cs
+proto = get_protocol_definition('PI30MST')
+#proto = Proto()
+
+
+class TestProtocolPi30Mst(unittest.TestCase):
+    """ exercise different functions in PI30MAX protocol """
+
+    def test_full_command_QPIGS2(self):
+        """ test a for correct full command for QPIGS2 """
+        _result = proto.get_full_command(command="QPIGS2")
+        expected = b'QPIGS2h-\r'
+        # print(_result)
+        self.assertEqual(_result, expected)
+
+    def test_build_result_QPIGS2(self):
+        """ test result build for QPIGS2 """
+        expected = ['pv2_input_current=3.1A current mdi:solar-power measurement', 'pv2_input_voltage=327.3V voltage mdi:solar-power measurement', 'battery_voltage_from_scc_2=52.3V voltage mdi:solar-power measurement', 'pv2_charging_power=123W power mdi:solar-power measurement', 'device_status=1', 'ac_charging_current=1234.0A current mdi:transmission-tower-export measurement', 'ac_charging_power=122W power mdi:transmission-tower-export measurement', 'pv3_input_current=327.1A current mdi:solar-power measurement', 'pv3_input_voltage=52.4V voltage mdi:solar-power measurement', 'battery_voltage_from_scc_3=234.0V voltage mdi:solar-power measurement', 'pv3_charging_power=567W power mdi:solar-power measurement']
+        simple_formatter = SimpleFormat({"extra_info": True})
+        device_info = DeviceInfo(name="name", serial_number="serial_number", model="model", manufacturer="manufacturer")
+        command = Command.from_config({"command": "QPIGS2"})
+        command.command_definition = proto.get_command_definition('QPIGS2')
+        _result = command.build_result(raw_response=b"(03.1 327.3 52.3 123 1 1234 122 327.1 52.4 234 567 \x23\xc7\r", protocol=proto)
+        formatted_data = simple_formatter.format(command, _result, device_info)
+        # print(formatted_data)
+        self.assertEqual(formatted_data, expected)
+
+
+    def test_get_id(self):
+        """ test get id command - should return QSID """
+        expected = "QSID"
+        res = proto.get_command_definition("get_id")
+        # print(res.code)
+        self.assertEqual(res.code, expected)
