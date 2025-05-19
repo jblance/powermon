66256a86cd582067739f4d5a45fc12085974afe9
John Blance <1266998+jblance@users.noreply.github.com>
2025-05-19 03:26:54 +0000

    sort daemon import approach

--- powermon/daemons/__init__.py
+++ powermon/daemons/__init__.py
@@ -1,40 +1,4 @@
-import logging
-from enum import StrEnum, auto
-
-from pydantic import BaseModel
-
 from ..config.daemon_config import DaemonConfig
+from .daemon import Daemon, DaemonDTO, DaemonType, daemon_from_config
 
-# Set-up logger
-log = logging.getLogger("daemon_systemd")
-
-
-class DaemonType(StrEnum):
-    """ Daemon types implemented """
-    DISABLED = auto()
-    SIMPLE = auto()
-    SYSTEMD = auto()
-    INITD = auto()
-
-
-class DaemonDTO(BaseModel):
-    """ data transfer ojbect model """
-    daemon_type: DaemonType
-    enabled: bool
-    keepalive: int
-
-
-def from_config(config: DaemonConfig):
-    match config.type:
-        case DaemonType.DISABLED | None:
-            from .daemon_disabled import DaemonDisabled as Daemon
-        case DaemonType.SIMPLE:
-            from .daemon_simple import DaemonSimple as Daemon
-        case DaemonType.SYSTEMD:
-            from .daemon_systemd import DaemonSystemd as Daemon
-        case DaemonType.INITD:
-            from .daemon_initd import DaemonInitd as Daemon
-        case _:
-            log.error('Invalid daemon type: %s', config.type)
-            return
-    return Daemon(keepalive=config.keepalive)
+__all__ = ['DaemonType', 'DaemonDTO', 'DaemonConfig', 'Daemon', 'daemon_from_config']
--- powermon/daemons/daemon.py
+++ powermon/daemons/daemon.py
@@ -1,12 +1,45 @@
 """ daemon.py """
 import logging
 from abc import ABC, abstractmethod
+from enum import StrEnum, auto
+from typing import Optional
 
-from . import DaemonDTO
+from pydantic import BaseModel
 
+from . import DaemonConfig
 
 # Set-up logger
 log = logging.getLogger("daemon")
+
+def daemon_from_config(config: DaemonConfig):
+    match config.type:
+        case DaemonType.DISABLED | None:
+            from .daemon_disabled import DaemonDisabled as daemon
+        case DaemonType.SIMPLE:
+            from .daemon_simple import DaemonSimple as daemon
+        case DaemonType.SYSTEMD:
+            from .daemon_systemd import DaemonSystemd as daemon
+        case DaemonType.INITD:
+            from .daemon_initd import DaemonInitd as daemon
+        case _:
+            log.error('Invalid daemon type: %s', config.type)
+            from .daemon_disabled import DaemonDisabled as daemon
+    return daemon(keepalive=config.keepalive)
+
+
+class DaemonType(StrEnum):
+    """ Daemon types implemented """
+    DISABLED = auto()
+    SIMPLE = auto()
+    SYSTEMD = auto()
+    INITD = auto()
+
+
+class DaemonDTO(BaseModel):
+    """ data transfer ojbect model """
+    daemon_type: DaemonType
+    last_notify: Optional[int | float] = None
+    keepalive: int
 
 
 class Daemon(ABC):
@@ -15,10 +48,10 @@ class Daemon(ABC):
         return f"Daemon: {self.__class__=}, {self.keepalive=}, {self.last_notify}"
 
 
-    def __init__(self, keepalive):
-        self.type = None
-        self.keepalive = keepalive
-        self.last_notify = None
+    def __init__(self, keepalive: int):
+        self.type: DaemonType = None
+        self.keepalive: int  = keepalive
+        self.last_notify: Optional[int | float] = None
 
 
     @abstractmethod
