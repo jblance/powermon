464e824c02fbb3457ecfbc1136474d05be3817a1
John Blance <1266998+jblance@users.noreply.github.com>
2026-04-09 08:18:51 +0000

    remove duplicates

--- powermon/configmodel/__init__.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Extra, Field
-
-
-class NoExtraBaseModel(BaseModel):
-    """ updated BaseModel with Extras forbidden """
-    class Config:
-        """pydantic BaseModel config"""
-        extra = Extra.forbid
-        
